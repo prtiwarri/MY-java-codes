14dcf135cafd64ed82cd03f489dd846620bad941
prtiwarri <144730715+prtiwarri@users.noreply.github.com>
2023-09-14 19:39:27 +0530

    Add files via upload
    This code allows the user to print their name and can help in understanding in how to write the java programm.

---
+++ First.java
@@ -0,0 +1,19 @@
+import java.lang.*;
+import java.util.*;  
+
+class First
+{
+            public static void main(String args[])
+            {
+                   String = First.main
+
+            
+                   Scanner sc = Scanner(System.in);
+                              
+
+                   System.out.println("May I know Your Name");
+                  string = sc.nextLine();
+  
+                  
+             }
+}
